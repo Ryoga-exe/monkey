fb19efb76f93083d44e3a153136569b3263b702c
Ryoga-exe <git@ryoga.dev>
2024-01-15 15:08:02 +0900

    feat: add token consts

--- internal/token/token.go
+++ internal/token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier and Literal
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)

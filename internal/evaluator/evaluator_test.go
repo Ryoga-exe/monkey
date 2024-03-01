6cd8655802fa2ddfb2f3cee746ee421aec931b70
Ryoga-exe <git@ryoga.dev>
2024-03-01 20:33:30 +0900

    feat: implement string

--- internal/evaluator/evaluator_test.go
+++ internal/evaluator/evaluator_test.go
@@ -60,6 +60,8 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"(1 < 2) == false", false},
 		{"(1 > 2) == true", false},
 		{"(1 > 2) == false", true},
+		{`"foobar" == "foobar"`, true},
+		{`"foo bar" == "foobar"`, false},
 	}
 
 	for _, tt := range tests {
@@ -70,6 +72,20 @@ func TestEvalBooleanExpression(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
@@ -267,6 +283,10 @@ if (10 > 1) {
 		{
 			"foobar",
 			"identifier not found: foobar",
+		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 

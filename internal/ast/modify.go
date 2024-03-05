91136fcc7443eb7ab7387149ce784d9a9ce9832a
Ryoga-exe <git@ryoga.dev>
2024-03-06 06:12:31 +0900

    feat: add modifier for BlockStatement and IfExpression

--- internal/ast/modify.go
+++ internal/ast/modify.go
@@ -4,12 +4,18 @@ type ModifierFunc func(Node) Node
 
 func Modify(node Node, modifier ModifierFunc) Node {
 	switch node := node.(type) {
+	// Statements
 	case *Program:
 		for i, statement := range node.Statements {
 			node.Statements[i], _ = Modify(statement, modifier).(Statement)
 		}
 	case *ExpressionStatement:
 		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	case *BlockStatement:
+		for i, statement := range node.Statements {
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+	// Expressions
 	case *InfixExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Right, _ = Modify(node.Right, modifier).(Expression)
@@ -18,6 +24,12 @@ func Modify(node Node, modifier ModifierFunc) Node {
 	case *IndexExpression:
 		node.Left, _ = Modify(node.Left, modifier).(Expression)
 		node.Index, _ = Modify(node.Left, modifier).(Expression)
+	case *IfExpression:
+		node.Condition, _ = Modify(node.Condition, modifier).(Expression)
+		node.Consequence, _ = Modify(node.Consequence, modifier).(*BlockStatement)
+		if node.Alternative != nil {
+			node.Alternative, _ = Modify(node.Alternative, modifier).(*BlockStatement)
+		}
 	}
 
 	return modifier(node)
